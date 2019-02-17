4df4770943c70725fc70f554c8c1042db1a55bbe
valeriapaolucci <valeriapaolucci2@gmail.com>
2019-02-18 00:08:19 +0100

    previous recursive implementation of clear function (probably not worth it)

--- clear.cc
+++ clear.cc
@@ -9,10 +9,10 @@ void BinTree<T,V>::clear(){
 
 
 template <typename T, typename V>
-void BinTree<T,V>::rclear(Node* n){																//recursive function, to clear the content of the tree
+void BinTree<T,V>::rclear(Node* n){															//recursive function, to clear the content of the tree
 	if (n->left != nullptr)
 		rclear(n->left);
 	if (n->right != nullptr)
 		rclear(n->right);		
- 	n.reset(nullptr);																								// == n.reset() ???
+ 	n.reset(nullptr);																							// == n.reset() ???
 }																																									
